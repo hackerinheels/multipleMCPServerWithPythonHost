159d42fe9970a028016f94b69e85a53221ef67fc
hackerinheels <gagarwal.jobs@gmail.com>
2025-03-27 15:38:01 -0700

    Add read from file server

---
+++ readFile/readFile.py
@@ -0,0 +1,65 @@
+from typing import Any, Dict, List 
+from mcp.server.fastmcp import FastMCP
+from starlette.applications import Starlette
+from sse_starlette.sse import EventSourceResponse
+from mcp.server.sse import SseServerTransport
+from starlette.requests import Request
+from starlette.routing import Mount, Route
+from mcp.server import Server
+import uvicorn
+import os
+# Initialize FastMCP server
+mcp = FastMCP("Read content of a file")
+
+
+@mcp.tool("read_from_csv_file")
+async def get_csv_file_content(file_path: str) -> List[Dict[str, str]]:
+    """
+    Returns contents of a csv file.
+    """
+    parentDir = os.path.dirname(os.getcwd())
+    currentDir = os.path.basename(os.getcwd())
+    file_path = os.path.join(parentDir, currentDir, file_path)
+    print("Readig from" , file_path)
+    with open(file_path, 'r') as file:
+        print(f"File content: {file.read()}")
+        return file.read()
+
+
+def create_starlette_app(mcp_server: Server, *, debug: bool = False) -> Starlette:
+    """Create a Starlette application that can server the provied mcp server with SSE."""
+    sse = SseServerTransport("/messages/")
+
+    async def handle_sse(request: Request) -> None:
+        async with sse.connect_sse(
+                request.scope,
+                request.receive,
+                request._send,  # noqa: SLF001
+        ) as (read_stream, write_stream):
+            await mcp_server.run(
+                read_stream,
+                write_stream,
+                mcp_server.create_initialization_options(),
+            )
+
+    return Starlette(
+        debug=debug,
+        routes=[
+            Route("/sse", endpoint=handle_sse),
+            Mount("/messages/", app=sse.handle_post_message),
+        ],
+    )
+
+
+if __name__ == "__main__":
+    mcp_server = mcp._mcp_server  # noqa: WPS437
+    import argparse
+    parser = argparse.ArgumentParser(description='Run MCP SSE-based server')
+    parser.add_argument('--host', default='0.0.0.0', help='Host to bind to')
+    parser.add_argument('--port', type=int, default=8081, help='Port to listen on')
+    args = parser.parse_args()
+
+    # Bind SSE request handling to MCP server
+    starlette_app = create_starlette_app(mcp_server, debug=True)
+
+    uvicorn.run(starlette_app, host=args.host, port=args.port)
